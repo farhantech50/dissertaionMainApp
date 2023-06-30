dc8da1fd97497079ea8a4180d1a753a97dfb166e
farhan1611078 <1611078@iub.edu.bd>
2023-06-30 02:56:22 +0100

    firstVersion

--- public/submit.js
+++ public/submit.js
@@ -1,13 +1,13 @@
 function sendData(){
     const ejsData = document.getElementById('ejsData').innerText; //getting the ejs passed data from html
     if(ejsData=='get'){
-        fetch(`http://localhost:5000/${document.getElementById('param1').value}/${document.getElementById('param2').value}`).then(res=>res.json()).then((data)=>{
+        fetch(`https://lbunodejsendpoint.azurewebsites.net/${document.getElementById('param1').value}/${document.getElementById('param2').value}`).then(res=>res.json()).then((data)=>{
             const result = document.getElementById('result');
             result.value = `Brand: ${data.brand} `+'\n'+ `Model: ${data.model}` 
         })
     }
     if(ejsData=='post'){
-        fetch('http://localhost:5000/', {
+        fetch("https://lbunodejsendpoint.azurewebsites.net/", {
         method: 'POST',
         headers: {
         'Accept': 'application/json, text/plain, */*',
@@ -31,12 +31,12 @@ function selectedRadio(){
     
     if(selected=='params'){
         document.getElementById('codeBox').value = `const express = require("express");`+'\n'+`const app  = express();`+'\n'+`app.listen(Port,()=>{`+'\n'+`    console.log('Server has been started at port ____')`+'\n'+`})`+'\n'+`app.get('/:brand/:watt',(req,res)=>{`+'\n'+`    if(req.params.brand=='longi'){`+'\n'+`    //Code to solve logical operations`+'\n'+`    }`+'\n'+`    res.send({"brand":value1,"model":value2});`+'\n'+`})`;
-        document.getElementById('test').innerHTML = '<label for="param1">http://localhost:5000/&#160</label> <input type="text" id="param1" size="3"> <label for="param2">&#160/&#160</label> <input type="number" id="param2" style="width: 3.5em">';
+        document.getElementById('test').innerHTML = '<label for="param1">https://lbunodejsendpoint.azurewebsites.net/</label> <input type="text" id="param1" size="3"> <label for="param2">&#160/&#160</label> <input type="number" id="param2" style="width: 3.5em">';
         document.getElementById('note').value = `Enter the value in the respective fields to send GET request for the above API code.`+'\n'+`*For the first box use brand name: Longi/Trina/JASolar`+'\n'+`*For the second box use watt rating: 400/500/600`
     }
     if(selected=='query'){
         document.getElementById('codeBox').value = `const express = require("express");`+'\n'+`const app  = express();`+'\n'+`app.listen(Port,()=>{`+'\n'+`    console.log('Server has been started at port ____')`+'\n'+`})`+'\n'+`app.get('/?',(req,res)=>{`+'\n'+`    if(req.query.brand=='longi'){`+'\n'+`    //Code to solve logical operations`+'\n'+`    }`+'\n'+`    res.send({"brand":value1,"model":value2});`+'\n'+`})`;
-        document.getElementById('test').innerHTML = '<label for="param1">http://localhost:5000/?brand=&#160</label> <input type="text" id="param1" size="3">&#160&watt=&#160</input> <input type="number" id="param2" style="width: 3.5em">'
+        document.getElementById('test').innerHTML = '<label for="param1">https://lbunodejsendpoint.azurewebsites.net/?brand=&#160</label> <input type="text" id="param1" size="3">&#160&watt=&#160</input> <input type="number" id="param2" style="width: 3.5em">'
         document.getElementById('note').value = `Enter the value in the respective fields to send GET request for the above API code.`+'\n'+`*For the first box use brand name: Longi/Trina/JASolar`+'\n'+`*For the second box use watt rating: 400/500/600`
     }
     if(selected=='body'){
